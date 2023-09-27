b10d2b8316e9a26909504f505230ebc24f6b1eb4
maria-studiodialect <maria@studiodialect.com>
2023-09-27 17:34:17 +0100

    main

--- src/app/page.js
+++ src/app/page.js
@@ -6,38 +6,71 @@ import {remove} from "next/dist/build/webpack/loaders/resolve-url-loader/lib/fil
 
 export default function Home() {
     const [users, setUsers] = useState([]); // Initialize with an empty array
+    const [mainUser, setMainUser] = useState(null); // Initialize with an empty array
 
-  useEffect(() => {
+
+    useEffect(() => {
     socket.emit('UsersRequest');
     socket.on('User List', (userList) => {
       setUsers(userList)
     }); 
   }, []);
 
+    useEffect(() => {
+        socket.on('Main User', (user) => {
+            console.log('received message!')
+            setMainUser(user)
+        });
+    }, []);
+
   function removeUser(id) {
       socket.emit('Remove User', id);
   }
 
-  console.log(users)
+    function removeMainUser(id) {
+        console.log(id)
+        socket.emit('Remove Main User', id);
+    }
+
+    function makeActive(id) {
+        socket.emit('Make Active', id);
+    }
+
+  console.log('userList ' + users)
 
   return (
-    <div>
-      <div className='mb-2 text-2xl font-bold'>User List</div>
-      <div>
-      {users.length > 0 ?
-      users.map((user, i) => (
-        <div key={i} className='border my-2 md:w-1/3 rounded-lg py-2 px-4 flex justify-between'>
-          {user}
-            <div>
-                {i > 0 && <span className='underline mr-2 cursor-pointer'>Make active</span>}
-                <span className='underline mr-2 text-red-500 cursor-pointer' onClick={() => removeUser(user)}>Delete</span>
+      <div className='md:grid grid-cols-2 gap-10'>
+        <div>
+          <div className='mb-2 text-2xl font-bold'>User List</div>
+          <div>
+          {users.length > 0 ?
+          users.map((user, i) => (
+            <div key={i} className='border my-2 md:w-full rounded-lg py-2 px-4 flex justify-between'>
+              {user}
+                <div>
+                    <span className='underline mr-2 cursor-pointer'  onClick={() => makeActive(user)}>Make active</span>
+                    <span className='underline mr-2 text-red-500 cursor-pointer' onClick={() => removeUser(user)}>Delete</span>
+                </div>
             </div>
+          ))
+              :
+              <div>No active users</div>
+          }
+          </div>
         </div>
-      ))
-          :
-          <div>No active users</div>
-      }
+          <div>
+              <div className='mb-2 text-2xl font-bold'>
+                  Active User
+              </div>
+              {mainUser ?
+              <div className='border my-2 md:w-full rounded-lg py-2 px-4 flex justify-between'>
+                  {mainUser}
+                  <span className='underline mr-2 text-red-500 cursor-pointer' onClick={() => removeMainUser(mainUser)}>Delete</span>
+              </div>
+                  :
+                  <div>No active user</div>
+              }
+          </div>
       </div>
-    </div>
   )
 }
